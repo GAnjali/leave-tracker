5183f283c2d9dfeb2caeff70a968c38fa6cfcdfd
Gaddam Anjali <anjali.gaddam98@gmail.com>
2021-03-22 19:36:25 +0530

    Implement add new leave functionality

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -2,7 +2,27 @@ class LeavesController < ApplicationController
   def index
     @leaves = Leave.all
   end
+
   def show
     @leave = Leave.find(params[:id])
+  end
+
+  def new
+    @leave = Leave.new
+  end
+
+  def create
+    @leave = Leave.new(leave_params)
+
+    if @leave.save
+      redirect_to @leave
+    else
+      render :new
+    end
+  end
+
+  private
+  def leave_params
+    params.require(:leave).permit(:start_date, :end_date)
   end
 end
