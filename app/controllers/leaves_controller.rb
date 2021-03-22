1ff6f49b6404159409b78d5284a8ad4b554a3428
Gaddam Anjali <anjali.gaddam98@gmail.com>
2021-03-22 19:23:36 +0530

    Add route for showing leave and implement link_to leave

--- app/controllers/leaves_controller.rb
+++ app/controllers/leaves_controller.rb
@@ -2,4 +2,7 @@ class LeavesController < ApplicationController
   def index
     @leaves = Leave.all
   end
+  def show
+    @leave = Leave.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   root 'leaves#index'
-  resource :leaves
+
+  resources :leaves
 end
