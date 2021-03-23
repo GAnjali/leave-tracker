5a1eb3addd3dd10bff2ad3e1df31994e1b84a6a0
Gaddam Anjali <anjali.gaddam98@gmail.com>
2021-03-23 16:28:33 +0530

    Refactore code

--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,9 @@
 require_relative "application"
 
 # Initialize the Rails application.
+
 Rails.application.initialize!
+
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root 'leaves#index'
+  root 'pages#home'
 
   resources :leaves
 end
